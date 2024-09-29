74234d8a4c7a11b2334264e7b329820afe43012a
yoshida keisuke <keeeesukee@gmail.com>
2024-09-30 08:38:46 +0900

    add

--- src/ekf_localization_component.cpp
+++ src/ekf_localization_component.cpp
@@ -14,39 +14,56 @@ using namespace Eigen;
 xtendedKalmanFilterComponent::ExtendedKalmanFilter(const rclcpp::NodeOptions & options)
     : Node("extended_kalman_filter", options)
 {
-    declare_parameter("pub_period", 10);
-    get_parameter("pub_period", pub_period_);
+  declare_parameter("reference_frame_id", "map");
+  declare_parameter("robot_frame_id", "base_link");
+  declare_parameter("initial_pose_topic","/initial_pose");
+  declare_parameter("imu_topic","/imu");
+  declare_parameter("odom_topic","/odom");
+  get_parameter("reference_frame_id", reference_frame_id_);
+  get_parameter("robot_frame_id", robot_frame_id_);
+  get_parameter("initial_pose_topic", initial_pose_topic_);
+  get_parameter("imu_topic", imu_topic_);
+  get_parameter("odom_topic", odom_topic_);
 
-    X << 0, 0, 0; // 初期位置と姿勢
-    P = Matrix3d::Identity(); // 初期共分散行列
+  declare_parameter("pub_period", 10);
+  declare_parameter("var_imu_w", 0.01);
+  declare_parameter("var_imu_acc", 0.01);
+  declare_parameter("var_odom_xyz", 0.2);
+  get_parameter("pub_period", pub_period_);
+  get_parameter("var_imu_w", var_imu_w_);
+  get_parameter("var_imu_acc", var_imu_acc_);
+  get_parameter("var_odom_xyz", var_odom_xyz_);
+ 
+  X << 0, 0, 0; // 初期位置と姿勢
+  P = Matrix3d::Identity(); // 初期共分散行列
 
-    double Q_x = 3;
-    Q << Q_x, 0, 0, 0, 0, 0, 0, M_x, 0, 0, 0, 0, 0, 0, M_x, 0, 0, 0, 0, 0, 0, M_x, 0, 0, 0, 0, 0, 0,
-        M_x, 0, 0, 0, 0, 0, 0, M_x;
-        
-    double M_x = 50;
-    R << R_x, 0, 0, 0, 0, 0, 
-         0, R_x, 0, 0, 0, 0, 
-         0, 0, R_x, 0, 0, 0, 
-         0, 0, 0, 10 * R_x, 0, 0, 
-         0, 0, 0, 0, 10 * R_x, 0, 
-         0, 0, 0, 0, 0, 10 * R_x;
+  double Q_x = 3;
+  Q << Q_x, 0, 0, 0, 0, 0, 0, M_x, 0, 0, 0, 0, 0, 0, M_x, 0, 0, 0, 0, 0, 0, M_x, 0, 0, 0, 0, 0, 0,
+      M_x, 0, 0, 0, 0, 0, 0, M_x;
+      
+  double M_x = 50;
+  R << R_x, 0, 0, 0, 0, 0, 
+        0, R_x, 0, 0, 0, 0, 
+        0, 0, R_x, 0, 0, 0, 
+        0, 0, 0, 10 * R_x, 0, 0, 
+        0, 0, 0, 0, 10 * R_x, 0, 
+        0, 0, 0, 0, 0, 10 * R_x;
 
-    // publisher
-    Posepublisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/estimated_pose", 10);
+  // publisher
+  Posepublisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/estimated_pose", 10);
 
-    // subscriber
-    imu_subscription_ = create_subscription<sensor_msgs::msg::Imu>(
-        "/imu", 10, [this](const sensor_msgs::msg::Imu::SharedPtr msg) {
-        odom_callback(msg);
-        });
+  // subscriber
+  imu_subscription_ = create_subscription<sensor_msgs::msg::Imu>(
+      "/imu", 10, [this](const sensor_msgs::msg::Imu::SharedPtr msg) {
+      odom_callback(msg);
+      });
 
-    odom_subscription_ = create_subscription<nav_msgs::msg::Odometry>(
-        "/odom", 10, [this](nav_msgs::msg::Odometry::SharedPtr msg) {
-        odom_callback(msg);
-        });
+  odom_subscription_ = create_subscription<nav_msgs::msg::Odometry>(
+      "/odom", 10, [this](nav_msgs::msg::Odometry::SharedPtr msg) {
+      odom_callback(msg);
+      });
 
-    timer_ = this->create_wall_timer(10ms, std::bind(&ExtendedKalmanFilterComponent::update, this));
+  timer_ = this->create_wall_timer(10ms, std::bind(&ExtendedKalmanFilterComponent::update, this));
 }
 
 
@@ -64,10 +81,78 @@ bool ExtendedKalmanFilterComponent::init()
   return initialized;
 }
 
+
 void ExtendedKalmanFilterComponent::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
-    y << msg->orientation.x, msg->orientation.y, msg->orientation.z;
-}
+  if (initial_pose_recieved_) {
+    sensor_msgs::msg::Imu transformed_msg;
+    try {
+      geometry_msgs::msg::Vector3S
+      tamped acc_in, acc_out, w_in, w_out;
+      acc_in.vector.x = msg->linear_acceleration.x;
+      acc_in.vector.y = msg->linear_acceleration.y;
+      acc_in.vector.z = msg->linear_acceleration.z;
+      w_in.vector.x = msg->angular_velocity.x;
+      w_in.vector.y = msg->angular_velocity.y;
+      w_in.vector.z = msg->angular_velocity.z;
+      tf2::TimePoint time_point = tf2::TimePoint(
+        std::chrono::seconds(msg->header.stamp.sec) +
+        std::chrono::nanoseconds(msg->header.stamp.nanosec));
+      const geometry_msgs::msg::TransformStamped transform =
+        tfbuffer_.lookupTransform(
+        robot_frame_id_,
+        msg->header.frame_id,
+        time_point);
+      tf2::doTransform(acc_in, acc_out, transform);
+      tf2::doTransform(w_in, w_out, transform);
+      transformed_msg.header.stamp = msg->header.stamp;
+      transformed_msg.angular_velocity.x = w_out.vector.x;
+      transformed_msg.angular_velocity.y = w_out.vector.y;
+      transformed_msg.angular_velocity.z = w_out.vector.z;
+      transformed_msg.linear_acceleration.x = acc_out.vector.x;
+      transformed_msg.linear_acceleration.y = acc_out.vector.y;
+      transformed_msg.linear_acceleration.z = acc_out.vector.z;
+      predictUpdate(transformed_msg);
+    } catch (tf2::TransformException & e) {
+      RCLCPP_ERROR(this->get_logger(), "%s", e.what());
+      return;
+    } catch (std::runtime_error & e) {
+      RCLCPP_ERROR(this->get_logger(), "%s", e.what());
+      return;
+    }
+  }
+};
+
+auto odom_callback =
+  [this](const typename nav_msgs::msg::Odometry::SharedPtr msg) -> void
+  {
+    if (initial_pose_recieved_ && use_odom_) {
+      Eigen::Affine3d affine;
+      tf2::fromMsg(msg->pose.pose, affine);
+      Eigen::Matrix4d odom_mat = affine.matrix();
+      if (previous_odom_mat_ == Eigen::Matrix4d::Identity()) {
+        current_pose_odom_ = current_pose_;
+        previous_odom_mat_ = odom_mat;
+        return;
+      }
+
+      Eigen::Affine3d current_affine;
+      tf2::fromMsg(current_pose_odom_.pose, current_affine);
+      Eigen::Matrix4d current_trans = current_affine.matrix();
+      current_trans = current_trans * previous_odom_mat_.inverse() * odom_mat;
+
+      geometry_msgs::msg::PoseStamped pose;
+      pose.header = msg->header;
+      pose.pose.position.x = current_trans(0, 3);
+      pose.pose.position.y = current_trans(1, 3);
+      pose.pose.position.z = current_trans(2, 3);
+      measurementUpdate(pose, var_odom_);
+
+      current_pose_odom_ = current_pose_;
+      previous_odom_mat_ = odom_mat;
+    }
+  };
+
 
 void ExtendedKalmanFilterComponent::odom_callback(const gnav_msgs::msg::Odometry::SharedPtr msg)
 {
@@ -142,7 +227,7 @@ void ExtendedKalmanFilterComponent::predict(const Vector2d& u)
             0, 0, 1;
 
     //3.事前誤差共分散行列の推定
-    P = G * P * G.transpose() + R;
+    P = (1 - G * P) * G.transpose() + R;
     X = X_pred;
 }
 
