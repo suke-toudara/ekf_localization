451dddbce323c6abe9cacdcf404a362e69e7a7d4
yoshida keisuke <keeeesukee@gmail.com>
2024-10-03 07:17:33 +0900

    fix update func

--- include/ekf_localization/ekf_localization_component.hpp
+++ include/ekf_localization/ekf_localization_component.hpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <chrono>
+#include <functional>
+#include <eigen3/Eigen/Core>
+
 #include <rclcpp/rclcpp.hpp>
-#include <eigen3/Eigen/Dense>
-#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "sensor_msgs/msg/imu.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include <tf2_ros/transform_broadcaster.h>
+
+using Eigen::MatrixXd;
 
 namespace ekf_localization
 {
@@ -14,38 +20,44 @@ public:
 
 private:
   bool initialized = false;
-  // 状態ベクトル (x, y, theta)
+
+  float v;
+  float theta;
   Eigen::Vector3d X; 
-  
-  // プロセスノイズ共分散行列
+  Eigen::Vector3d Z; 
+
+  // noise_pram
+  float var_imu_w_;
+  float var_imu_acc_;
+  float var_odom_xyz_;
   MatrixXd Q = MatrixXd::Zero(3, 3);
-  Q(0, 0) = 0.1;  // Process noise for x
-  Q(1, 1) = 0.1;  // Process noise for y
-  Q(2, 2) = 0.01; // Process noise for theta
-  
-  // 観測ノイズ共分散行列
   MatrixXd R = MatrixXd::Zero(3, 3);
-  R(0, 0) = 0.1;  // Process noise for x
-  R(1, 1) = 0.1;  // Process noise for y
-  R(2, 2) = 0.01; // Process noise for theta
+
   // 共分散行列
   MatrixXd P = MatrixXd::Zero(3, 3);
 
-  timestamp;
+  rclcpp::Time prev_time_;
   rclcpp::Time odomtimestamp;
   rclcpp::Time imutimestamp;
 
   void init();
-  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-  void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+  void odom_callback(const nav_msgs::msg::Odometry & msg);
+  void imu_callback(const sensor_msgs::msg::Imu & msg);
  
-  void predictUpdate(nav_msgs::msg::Odometry::SharedPtr msg);
-  void measurementUpdate(sensor_msgs::msg::Imu msg);
+  void predictUpdate(const nav_msgs::msg::Odometry & msg);
+  void measurementUpdate(const sensor_msgs::msg::Imu & msg);
+
+  tf2_ros::TransformBroadcaster broadcaster_;
+  bool broadcast_transform_;
+
+  std::string map_frame_id_;
+  std::string robot_frame_id_;
+  std::string initial_pose_topic_;
+  std::string ekf_pose_topic_;
+  std::string imu_topic_;
+  std::string odom_topic_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscription_;
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr posepublisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr ekf_pose_publisher_;
 };
 }  // namespace 
-
-#endif  
--- src/ekf_localization_component.cpp
+++ src/ekf_localization_component.cpp
@@ -1,11 +1,6 @@
-#include <chrono>
-#include <functional>
 #include <memory>
 #include <iostream>
-#include <eigen3/Eigen/Dense>
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/imu.hpp"
-#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
 #include <ekf_localization/ekf_localization_component.hpp>
 
 using namespace std::chrono_literals;
@@ -14,104 +9,125 @@ using namespace Eigen;
 namespace ekf_localization
 {
 ExtendedKalmanFilter::ExtendedKalmanFilter(const rclcpp::NodeOptions & options)
-    : Node("extended_kalman_filter", options)
+: Node("extended_kalman_filter", options), broadcaster_(this)
 {
-  declare_parameter("reference_frame_id", "map");
+  declare_parameter("map_frame_id", "map");
   declare_parameter("robot_frame_id", "base_link");
   declare_parameter("initial_pose_topic","/initial_pose");
+  declare_parameter("ekf_pose_topic","/estimated_pose");
   declare_parameter("imu_topic","/imu");
   declare_parameter("odom_topic","/odom");
-  get_parameter("reference_frame_id", reference_frame_id_);
+
+  get_parameter("map_frame_id", map_frame_id_);
   get_parameter("robot_frame_id", robot_frame_id_);
   get_parameter("initial_pose_topic", initial_pose_topic_);
+  get_parameter("ekf_pose_topic", ekf_pose_topic_);
   get_parameter("imu_topic", imu_topic_);
   get_parameter("odom_topic", odom_topic_);
 
-  declare_parameter("pub_period", 10);
-  declare_parameter("var_imu_w", 0.01);
+  declare_parameter("var_imu_w", 0.1);
   declare_parameter("var_imu_acc", 0.01);
-  declare_parameter("var_odom_xyz", 0.2);
-  get_parameter("pub_period", pub_period_);
+  declare_parameter("var_odom_xyz", 0.1);
   get_parameter("var_imu_w", var_imu_w_);
   get_parameter("var_imu_acc", var_imu_acc_);
   get_parameter("var_odom_xyz", var_odom_xyz_);
  
+  // declare_parameter("pub_period", 10);
+  // get_parameter("pub_period", pub_period_);
+  
+
   // publisher
-  posepublisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/estimated_pose", 10);
+  ekf_pose_publisher_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/estimated_pose", 10);
 
   // subscriber
   odom_subscription_ = create_subscription<nav_msgs::msg::Odometry>(
-      "/odom", 10, [this](nav_msgs::msg::Odometry::SharedPtr msg) {
+      odom_topic_, 10, [this](const nav_msgs::msg::Odometry & msg) {
       odom_callback(msg);
       });
   imu_subscription_ = create_subscription<sensor_msgs::msg::Imu>(
-      "/imu", 10, [this](const sensor_msgs::msg::Imu::SharedPtr msg) {
+      imu_topic_, 10, [this](const sensor_msgs::msg::Imu & msg) {
       imu_callback(msg);
       });
+  
+  P(0, 0) = 0;  
+  P(1, 1) = 0;  
+  P(2, 2) = 0;  
+  
+  // プロセスノイズ共分散行列
+  Q(0, 0) = var_odom_xyz_;  // Process noise for x
+  Q(1, 1) = var_odom_xyz_;  // Process noise for y
+  Q(2, 2) = var_odom_xyz_;  // Process noise for theta
+  
+  // 観測ノイズ共分散行列
+  R(0, 0) = var_imu_w_;   // Process noise for x
+  R(1, 1) = var_imu_w_;   // Process noise for y
+  R(2, 2) = var_imu_acc_; // Process noise for theta
 }
 
 
 void ExtendedKalmanFilter::init()
 {
-  X_ << 0, 0, 0;
+  X << 0, 0, 0;
   initialized = true;
 }
 
 
-void ExtendedKalmanFilter::odom_callback(nav_msgs::msg::Odometry::SharedPtr msg)
+void ExtendedKalmanFilter::odom_callback(const nav_msgs::msg::Odometry & msg)
 {
   if (!initialized) {
     init();
   } else {
-  measurementUpdate(msg);
+    nav_msgs::msg::Odometry odom_msg = msg; 
+    predictUpdate(odom_msg);
   }
 }
 
 
-void ExtendedKalmanFilter::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+void ExtendedKalmanFilter::imu_callback(const sensor_msgs::msg::Imu & msg)
 {
   if (!initialized) {
     init();
   } else {
-    sensor_msgs::msg::Imu transformed_msg;
-    try {
-      geometry_msgs::msg::Vector3Stamped acc_in, acc_out, w_in, w_out;
-      acc_in.vector.x = msg->linear_acceleration.x;
-      acc_in.vector.y = msg->linear_acceleration.y;
-      acc_in.vector.z = msg->linear_acceleration.z;
-      w_in.vector.x = msg->angular_velocity.x;
-      w_in.vector.y = msg->angular_velocity.y;
-      w_in.vector.z = msg->angular_velocity.z;
-      tf2::TimePoint time_point = tf2::TimePoint(
-        std::chrono::seconds(msg->header.stamp.sec) +
-        std::chrono::nanoseconds(msg->header.stamp.nanosec));
-      const geometry_msgs::msg::TransformStamped transform =
-        tfbuffer_.lookupTransform(
-        robot_frame_id_,
-        msg->header.frame_id,
-        time_point);
-      tf2::doTransform(acc_in, acc_out, transform);
-      tf2::doTransform(w_in, w_out, transform);
-      transformed_msg.header.stamp = msg->header.stamp;
-      transformed_msg.angular_velocity.x = w_out.vector.x;
-      transformed_msg.angular_velocity.y = w_out.vector.y;
-      transformed_msg.angular_velocity.z = w_out.vector.z;
-      transformed_msg.linear_acceleration.x = acc_out.vector.x;
-      transformed_msg.linear_acceleration.y = acc_out.vector.y;
-      transformed_msg.linear_acceleration.z = acc_out.vector.z;
-      measurementUpdate(transformed_msg);
-    } catch (tf2::TransformException & e) {
-      RCLCPP_ERROR(this->get_logger(), "%s", e.what());
-      return;
-    } catch (std::runtime_error & e) {
-      RCLCPP_ERROR(this->get_logger(), "%s", e.what());
-      return;
-    }
+    // sensor_msgs::msg::Imu transformed_msg;
+    // try {
+      // geometry_msgs::msg::Vector3Stamped acc_in, acc_out, w_in, w_out;
+      // acc_in.vector.x = msg->linear_acceleration.x;
+      // acc_in.vector.y = msg->linear_acceleration.y;
+      // acc_in.vector.z = msg->linear_acceleration.z;
+      // w_in.vector.x = msg->angular_velocity.x;
+      // w_in.vector.y = msg->angular_velocity.y;
+      // w_in.vector.z = msg->angular_velocity.z;
+      // tf2::TimePoint time_point = tf2::TimePoint(
+      //   std::chrono::seconds(msg->header.stamp.sec) +
+      //   std::chrono::nanoseconds(msg->header.stamp.nanosec));
+      // const geometry_msgs::msg::TransformStamped transform =
+      //   tfbuffer_.lookupTransform(
+      //   robot_frame_id_,
+      //   msg->header.frame_id,
+      //   time_point);
+      // tf2::doTransform(acc_in, acc_out, transform);
+      // tf2::doTransform(w_in, w_out, transform);
+      // transformed_msg.header.stamp = msg->header.stamp;
+      // transformed_msg.angular_velocity.x = w_out.vector.x;
+      // transformed_msg.angular_velocity.y = w_out.vector.y;
+      // transformed_msg.angular_velocity.z = w_out.vector.z;
+      // transformed_msg.linear_acceleration.x = acc_out.vector.x;
+      // transformed_msg.linear_acceleration.y = acc_out.vector.y;
+      // transformed_msg.linear_acceleration.z = acc_out.vector.z;
+    sensor_msgs::msg::Imu imu_msg = msg; 
+    measurementUpdate(imu_msg);
+    // } catch (tf2::TransformException & e) {
+    //   RCLCPP_ERROR(this->get_logger(), "%s", e.what());
+    //   return;
+    // } catch (std::runtime_error & e) {
+    //   RCLCPP_ERROR(this->get_logger(), "%s", e.what());
+    //   return;
+    // }
   }
 }
 
 
-void ExtendedKalmanFilter::predictUpdate(nav_msgs::msg::Odometry::SharedPtr msg)
+void ExtendedKalmanFilter::predictUpdate(const nav_msgs::msg::Odometry & msg)
 {
 
   // 予測ステップ
@@ -128,14 +144,15 @@ void ExtendedKalmanFilter::predictUpdate(nav_msgs::msg::Odometry::SharedPtr msg)
   */
 
   //1.運動モデルによる予測(事前推定値)
-  odomtimestamp = msg->header.stamp;
-  double dt = odomtimestamp - old_timestamp
-  old_timestamp = odomtimestamp;
-  X(0) = msg->pose.pose.position.x;
-  X(1) = msg->pose.pose.position.y;
-  X(2) = msg->pose.pose.position.z;
-  double v = msg->twist.twist.linear.z;
-  double theta = msg->twist.twist.angular.z;
+  odomtimestamp = msg.header.stamp;
+  rclcpp::Duration duration = odomtimestamp - prev_time_;
+  double dt = duration.seconds(); 
+  prev_time_ = odomtimestamp;
+  X(0) = msg.pose.pose.position.x;
+  X(1) = msg.pose.pose.position.y;
+  X(2) = msg.pose.pose.position.z;
+  v = msg.twist.twist.linear.z;
+  theta = msg.twist.twist.angular.z;
   //2.ヤコビアン計算
   Matrix3d F;
   F << 1, 0, -v*sin(theta)*dt ,
@@ -147,7 +164,7 @@ void ExtendedKalmanFilter::predictUpdate(nav_msgs::msg::Odometry::SharedPtr msg)
 
 }
 
-void ExtendedKalmanFilter::measurementUpdate(sensor_msgs::msg::Imu msg)
+void ExtendedKalmanFilter::measurementUpdate(const sensor_msgs::msg::Imu & msg)
 {   
   /*
   Update : 観測後の信念分布の更新
@@ -159,15 +176,16 @@ void ExtendedKalmanFilter::measurementUpdate(sensor_msgs::msg::Imu msg)
   */
 
   //1.観測予測値の計算
-  imutimestamp = msg->header.stamp;
-  double dt = imutimestamp - old_timestamp;
-  old_timestamp = imutimestamp;
+  imutimestamp = msg.header.stamp;
+  rclcpp::Duration duration = imutimestamp - prev_time_;
+  double dt = duration.seconds(); 
+  prev_time_ = imutimestamp;
 
-  double v += u(0)*dt
+  v += msg.linear_acceleration.x*dt;
   double x = X(0), y = X(1), theta = X(2);
   double dx = v*cos(theta);
   double dy = v*sin(theta);
-  double dtheta = u(5);
+  double dtheta = msg.angular_velocity.z;
   Z(0) = x + dx * dt;
   Z(1) = y + dy * dt;
   Z(2) = theta + dtheta * dt;
@@ -180,14 +198,46 @@ void ExtendedKalmanFilter::measurementUpdate(sensor_msgs::msg::Imu msg)
 
   //3.観測予測値の計算
   Eigen::MatrixXd h = Eigen::MatrixXd::Identity(3,3);
-  VectorXdy = Z - h * X;
+  Eigen::VectorXd Y = Z - h * X;
 
   //3.カルマンゲインの計算
   MatrixXd S = H * P * H.transpose() + R;
   MatrixXd K = P * H.transpose() * S.inverse();
   //3.観測予測値の計算
-  X = X + K * y;
+  X = X + K * Y;
   P = (Matrix3d::Identity() - K * H) * P;
+
+  // publish pose_ekf
+  geometry_msgs::msg::PoseWithCovarianceStamped pose_ekf;
+  pose_ekf.header.stamp = imutimestamp;
+  pose_ekf.header.frame_id = map_frame_id_;
+  pose_ekf.pose.pose.position.x = X(0);
+  pose_ekf.pose.pose.position.y = X(1);
+  pose_ekf.pose.pose.position.z = X(2);
+
+  // pose_ekf.pose.pose.orientation.w =;
+  // pose_ekf.pose.pose.orientation.x =;
+  // pose_ekf.pose.pose.orientation.y =;
+  // pose_ekf.pose.pose.orientation.z =;
+
+  pose_ekf.pose.covariance = {
+    P(0, 0), P(0, 1), P(0, 2),
+    P(1, 0), P(1, 1), P(1, 2),
+    P(2, 0), P(2, 1), P(2, 2),
+  };
+  ekf_pose_publisher_->publish(pose_ekf);
+
+  if (broadcast_transform_) {
+      geometry_msgs::msg::TransformStamped transform_stamped;
+      transform_stamped.header.frame_id = map_frame_id_;
+      transform_stamped.header.stamp = pose_ekf.header.stamp;
+      transform_stamped.child_frame_id = robot_frame_id_;
+      transform_stamped.transform.translation.x = pose_ekf.pose.pose.position.x;
+      transform_stamped.transform.translation.y = pose_ekf.pose.pose.position.y;
+      transform_stamped.transform.translation.z = pose_ekf.pose.pose.position.z;
+      transform_stamped.transform.rotation = pose_ekf.pose.pose.orientation;
+      broadcaster_.sendTransform(transform_stamped);
+  }
 }
 
 }
